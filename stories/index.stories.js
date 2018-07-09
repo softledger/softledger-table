77c52099f621e9e7df601033504f91435af05a2f
Geoff Ostrega <geoff.ostrega@gmail.com>
2018-07-08 17:32:50 -0700

    added styles for nested headers

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -178,6 +178,51 @@ storiesOf('SLTable', module)
 			</div>
 		);
 	}))
+	.add('with Nested Header', withState({
+		data: []
+	}, store => {
+		const fetchData = ({pageSize, page, sorted, filtered}) => fetch(` https://api.punkapi.com/v2/beers?page=${page+1}&per_page=${[pageSize]}`)
+			.then(r => r.json())
+			.then(data => 
+				store.set({
+					data
+				})
+			)
+
+		return (
+			<SLTable
+				fetchData={fetchData}
+				data={store.state.data}
+				pages={10} //not really sture how many
+				columns={[{
+					Header: 'ID',
+					accessor: 'id',
+					Footer: 
+						<div>
+							Footer Text!!!
+						</div>
+				}, {
+					Header: 'Details',
+					columns: [{
+						Header: 'Name',
+						accessor: 'name'
+					}, {
+						Header: 'Description',
+						accessor: 'description'
+					}, {
+						Header: 'ABV',
+						accessor: 'abv'
+					}, {
+						Header: 'IBU',
+						accessor: 'ibu'
+					}] 
+				}, {
+					Header: "Brewer's Tips",
+					accessor: 'brewers_tips'
+				}]}
+			/>
+		);
+	}))
 
 
 storiesOf('SelectTable', module)
